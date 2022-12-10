71d07f809021536927e31fd5ce89ca6e5968fba2
Tim Kent <timdekent@gmail.com>
2022-12-10 14:25:08 -0800

    update logo text styling

--- components/layout/Logo.js
+++ components/layout/Logo.js
@@ -7,7 +7,7 @@ const Logo = () => {
   return (
     // <div className="flex flex-col items-center justify-center h-[100px] bg-gradient-to-r from-gray-800 via-blue-700 to-gray-800">
     <div className="flex flex-col items-center justify-center h-[100px] xl:h-[130px] 2xl:h-[160px] bg-hero-pattern bg-no-repeat bg-cover bg-center">
-      <h1 className="header-text z-10 font-serif text-center text-lg min-[360px]:text-xl min-[375px]:text-2xl md:text-3xl xl:text-4xl 2xl:text-5xl text-white select-none">
+      <h1 className="header-text z-10 font-serif text-center text-lg min-[375px]:text-xl md:text-3xl xl:text-4xl 2xl:text-5xl text-white select-none">
         Historic Landmarks <span className="italic">of</span>{" "}
         <span className="text-sky-400">San Francisco</span>
       </h1>
